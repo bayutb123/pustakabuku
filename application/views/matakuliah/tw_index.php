a85180417117df7d6938948a614f51d397395f0f
bayutb123 <bayutantra28@gmail.com>
2023-04-01 11:16:45 +0700

    Pemindahan tombol save ke kanan

--- application/views/matakuliah/tw_index.php
+++ application/views/matakuliah/tw_index.php
@@ -68,8 +68,7 @@
             </div>
 
             <div class="md:flex md:items-center">
-                <div class="md:w-1/3"></div>
-                <div class="md:w-2/3">
+                <div class="relative">
                     <button class="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
                         Save
                     </button>
